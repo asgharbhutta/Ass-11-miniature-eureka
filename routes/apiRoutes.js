59092dd36b46c1761be6b8d3a5b48ca268a3bf2c
AsgharB <abhutta@Asghar.local>
2020-11-14 14:24:05 -0500

    final upload

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -20,7 +20,7 @@ router.post('/notes', (req, res) => {
 router.delete('/notes/:id', (req, res) => {
     store
         .removeNote(req.params.id)
-        .then(() => res.json({ok:true}))
+        .then(() => res.json({ ok: true }))
         .catch((err) => res.status(500).json(err));
 });
 
